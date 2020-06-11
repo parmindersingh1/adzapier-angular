01e90b8772717e18f0d287812d934ec2b6b9fc67
adityamishra <amishra@adzapier.com>
2020-06-11 12:12:25 +0530

    added stripePublishablekey in the env file

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   mock: false,
  
-  
+  stripePublishablekey : 'pk_test_Bea8DtMw2JDaQoZuvyd2yEdE00wtdPEyoM',
   apiUrl:"//develop-cmp-api.adzpier.com/api/v1"
  
 };
