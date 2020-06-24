4b97eb26b1e67d6689633d8e402c6104fdfb45bd
Milan  Panchal <mpanchal@adzapier.com>
2020-06-24 22:41:22 +0530

    remove timeAgo pipe

--- src/app/pagesettings/organizationteam/organizationteam.module.ts
+++ src/app/pagesettings/organizationteam/organizationteam.module.ts
@@ -5,7 +5,7 @@ import { OrganizationteamRoutingModule } from './organizationteam-routing.module
 import { OrganizationteamComponent } from './organizationteam.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedbootstrapModule } from 'src/app/sharedbootstrap/sharedbootstrap.module';
-//import {TimeAgoPipe} from 'time-ago-pipe';
+// import {TimeAgoPipe} from 'time-ago-pipe';
 
 @NgModule({
   declarations: [OrganizationteamComponent ],
