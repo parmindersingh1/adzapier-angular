a03200200851662f858580d6e6a67d905c02aed2
Aditya Mishra <amishra@adzapier.com>
2020-11-11 15:58:27 +0530

    fixed some issues

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { SharedbootstrapModule } from 'src/app/sharedbootstrap/sharedbootstrap.module';
-import {NgxPasswordToggleModule} from 'ngx-password-toggle';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AlertModule} from 'ngx-bootstrap';
 
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { SharedbootstrapModule } from 'src/app/sharedbootstrap/sharedbootstrap.module';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
-import {NgxPasswordToggleModule} from 'ngx-password-toggle';
 import {AlertModule} from 'ngx-bootstrap';
 
 @NgModule({
@@ -15,8 +13,6 @@ import {AlertModule} from 'ngx-bootstrap';
     FormsModule,
     RegisterRoutingModule,
     AlertModule,
-    // SharedbootstrapModule,
-    // NgxPasswordToggleModule
   ]
 })
 export class RegisterModule { }
