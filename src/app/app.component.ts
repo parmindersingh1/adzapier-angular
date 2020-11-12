ca434ff1cdd520a8a43c609c584f85418e16275c
Milan  Panchal <mpanchal@adzapier.com>
2020-11-12 23:39:50 +0530

    #AP-759,AP-766 remove green color from team table, Reset button for webform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { AuthenticationService } from './_services';
+import { AuthenticationService, OrganizationService } from './_services';
 import { User } from './_models';
 import { HeaderComponent } from './_components/layout/header/header.component';
 import { Router, NavigationEnd } from '@angular/router';
 import * as feather from 'feather-icons';
+import { CCPAFormConfigurationService } from './_services/ccpaform-configuration.service';
+import { DsarformService } from './_services/dsarform.service';
 
 @Component({
   selector: 'app-root',
@@ -18,15 +20,27 @@ export class AppComponent implements OnInit {
   title = 'adzapier-analytics-ng';
   faCoffee = faCoffee;
   hideHeaderFooter = true;
-  constructor(private router: Router) {
-  //  this.headerComponent.loadOrganizationList();
+  constructor(private router: Router, 
+              private authenticationService: AuthenticationService,
+              private ccpaFormConfigurationService: CCPAFormConfigurationService,
+              private dsarformService: DsarformService,
+              private organizationService: OrganizationService
+  ) {
+    //  this.headerComponent.loadOrganizationList();
   }
 
   ngOnInit() {
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.hideHeaderFooter = (event.url.indexOf('/editwebforms') === -1);
+        if (event.url.indexOf('/resetpswd') !== -1 || event.url.indexOf('/verify-email') !== -1) {
+          this.hideHeaderFooter = true;
+          this.authenticationService.logout();
+          this.ccpaFormConfigurationService.removeControls();
+          this.dsarformService.removeControls();
+          this.organizationService.removeControls();
+        }
+
       }
     });
     feather.replace();
