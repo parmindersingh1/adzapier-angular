ee485215b0b8bf250934373eaf0861a81ec15ec6
Krunal Binekar <kbinekar@adzapier.com>
2021-04-01 14:58:16 +0530

    consent solution API integrate

--- src/app/privacy/consent_solutions/consent-solutions/consent-solutions.component.ts
+++ src/app/privacy/consent_solutions/consent-solutions/consent-solutions.component.ts
@@ -1,4 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export class legal_notices{
+  constructor(
+    public id:number,
+    public consent_id:number,
+    public version:number,
+    public identifier:string,
+    public created_at:any,
+    public updated_at:any
+
+  ){
+
+  }
+}
+
+export class consent{
+  constructor(
+    public id:number,
+    public owner_id:any,
+    public email:any,
+    public first_name:any,
+    public last_name:any,
+    public verified:boolean,
+    public ip_address:any,
+    public data_source:any,
+    public LegalNotice:any,
+    public Proofs:any,
+    public Preference:any,
+    public created_at:any,
+    public updated_at:any
+
+  ){
+
+  }
+}
+
 
 @Component({
   selector: 'app-consent-solutions',
@@ -6,22 +43,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consent-solutions.component.scss']
 })
 export class ConsentSolutionsComponent implements OnInit {
-  data:Array<any>;
-  constructor() {
-    this.data = [
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key',identifier : 'privacy-policy',version:'40'},
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key' ,identifier : 'Cookie-policy',version:'40'},
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key',identifier : 'privacy-policy',version:'40' },
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key' ,identifier : 'privacy-policy',version:'40'},
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key' ,identifier : 'privacy-policy',version:'40'},
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key' ,identifier : 'privacy-policy',version:'40'},
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key' ,identifier : 'privacy-policy',version:'40'},
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key' ,identifier : 'privacy-policy',version:'40'},
-      { creation: '2019/06/23 18:07:46', subject: 'A@adz.com', source: 'public key' ,identifier : 'privacy-policy',version:'40'}
-  ];
+  legal_notice : any;
+  legal_notices=[];
+
+  consent:any;
+   consents=[];
+
+
+  constructor(private httpClient:HttpClient) {
+   
    }
 
   ngOnInit() {
+    this.getlegal_notices();
+    this.getConsent();
+
   }
 
+  getlegal_notices(){
+    this.httpClient.get<any>('https://develop-cmp-api.adzpier-staging.com/api/v1/legal_notices').subscribe(
+      response =>{
+        console.log(response);
+        this.legal_notice=response;
+      this.legal_notices = this.legal_notice.response;      }
+    )
+  }
+
+  getConsent(){
+    this.httpClient.get<any>('https://develop-cmp-api.adzpier-staging.com/api/v1/consent').subscribe(
+      response => {
+        console.log(response);
+        this.consent=response;
+        this.consents=this.consent.response;
+      }
+    )
+  }
 }
