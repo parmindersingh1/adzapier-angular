fc0f32e88c9d81109afbb82af6b940d3c8156b33
Aditya Mishra <amishra@adzapier.com>
2021-07-21 21:34:32 +0530

    fixed billing

--- src/app/pagesettings/billing/pricing/pricing.component.ts
+++ src/app/pagesettings/billing/pricing/pricing.component.ts
@@ -110,31 +110,34 @@ export class PricingComponent implements OnInit, OnDestroy {
   }
 
   onSelectActivePlan(record) {
-    const activeData = this.activeData;
-    for (const data of record) {
-      if (data.active && data.planDetails.type === 0) {
-        if (data.planDetails.price > activeData.cookieConsent.maxPrice) {
-          activeData.cookieConsent.maxPrice = data.planDetails.price;
-          activeData.cookieConsent.maxPlanID = data.planDetails.stripe_plan_id;
-          activeData.cookieConsent.cycle = data.planDetails.cycle;
-          this.cookieConsentBillingCycle = data.planDetails.cycle;
-        }
-      } else if (data.planDetails.price > activeData.dsar.maxPrice && data.active && data.planDetails.type === 1) {
-        activeData.dsar.maxPrice = data.planDetails.price;
-        activeData.dsar.maxPlanID = data.planDetails.stripe_plan_id;
-        activeData.dsar.cycle = data.planDetails.cycle;
-        this.addonsBillingCycle = data.planDetails.cycle;
-      } else if (data.planDetails.price > activeData.consentPreference.maxPrice && data.active && data.planDetails.type === 2) {
-        activeData.consentPreference.maxPrice = data.planDetails.price;
-        activeData.consentPreference.maxPlanID = data.planDetails.stripe_plan_id;
-        activeData.consentPreference.cycle = data.planDetails.cycle;
+    try {
+      const activeData = this.activeData;
+      for (const data of record) {
+        if (data.active && data.planDetails.type === 0) {
+          if (data.planDetails.price > activeData.cookieConsent.maxPrice) {
+            activeData.cookieConsent.maxPrice = data.planDetails.price;
+            activeData.cookieConsent.maxPlanID = data.planDetails.stripe_plan_id;
+            activeData.cookieConsent.cycle = data.planDetails.cycle;
+            this.cookieConsentBillingCycle = data.planDetails.cycle;
+          }
+        } else if (data.planDetails.price > activeData.dsar.maxPrice && data.active && data.planDetails.type === 1) {
+          activeData.dsar.maxPrice = data.planDetails.price;
+          activeData.dsar.maxPlanID = data.planDetails.stripe_plan_id;
+          activeData.dsar.cycle = data.planDetails.cycle;
+          this.addonsBillingCycle = data.planDetails.cycle;
+        } else if (data.planDetails.price > activeData.consentPreference.maxPrice && data.active && data.planDetails.type === 2) {
+          activeData.consentPreference.maxPrice = data.planDetails.price;
+          activeData.consentPreference.maxPlanID = data.planDetails.stripe_plan_id;
+          activeData.consentPreference.cycle = data.planDetails.cycle;
 
+        }
       }
+      this.activeData = activeData;
+      this.subscriptionList = this.planDetails.cookieConsent[`${this.cookieConsentBillingCycle}`];
+      this.dsarPlanList = this.planDetails.dsar[`${this.addonsBillingCycle}`];
+      this.consentPreferenceList = this.planDetails.consentPreference[`${this.addonsBillingCycle}`];
+    } catch (e) {
     }
-    this.activeData = activeData;
-    this.subscriptionList = this.planDetails.cookieConsent[`${this.cookieConsentBillingCycle}`];
-    this.dsarPlanList = this.planDetails.dsar[`${this.addonsBillingCycle}`];
-    this.consentPreferenceList = this.planDetails.consentPreference[`${this.addonsBillingCycle}`];
   }
 
   onGetPlanDetails() {
