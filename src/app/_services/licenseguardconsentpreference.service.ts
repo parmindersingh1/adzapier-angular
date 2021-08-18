56d0d4cced1e5abf3a891489ded84305090e846e
mpanchal <mpanchal@adzapier.com>
2021-08-18 15:49:33 +0530

    #AP-1817 manage properties in new tab check for license availabilty

--- src/app/_services/licenseguardconsentpreference.service.ts
+++ src/app/_services/licenseguardconsentpreference.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { findPropertyIDFromUrl } from '../_helpers/common-utility';
 import { DataService } from './data.service';
+import { Location } from '@angular/common';
 @Injectable({
     providedIn: 'root'
 })
 export class LicenseGuardConsentPreferenceService implements CanActivate {
 
-    constructor(private dataService: DataService) { }
+    constructor(private location: Location, private router: Router,private dataService: DataService) { }
 
-    canActivate(): boolean {
+    async canActivate(): Promise<boolean> {
         let consentPrefrenceForProperty;
 
         this.dataService.isConsentPreferenceAppliedForProperty.subscribe((status) => {
@@ -16,8 +18,34 @@ export class LicenseGuardConsentPreferenceService implements CanActivate {
         });
         if (consentPrefrenceForProperty.hasaccess){
             return true;
-        }else{
-            return false;
+        } else {
+            let oIDPIDFromURL = findPropertyIDFromUrl(this.location.path())
+            let results = await this.dataService.checkLicenseAvailabilityForProperty(oIDPIDFromURL[0]).toPromise();
+            if (results) {
+                let finalObj = {
+                    ...results[0].response
+                }
+                if (finalObj !== undefined && finalObj.plan_details && finalObj.plan_details) {
+                    if (Object.values(finalObj.plan_details.consentPreference).length > 0) {
+                      return true;
+                    } else {
+                      this.dataService.openUpgradeModalForConsentPreference(results[0])
+                      this.router.navigate(['/home/dashboard/analytics'], { queryParams: { oid: oIDPIDFromURL[0], pid: oIDPIDFromURL[1] }, skipLocationChange: false });
+                      return false;
+                    }
+                  }
+
+                  // if (resData.response && resData.response.plan_details && resData.response.plan_details.consentPreference) {
+  //   if (Object.values(resData.response.plan_details.consentPreference).length > 0) {
+  //     this.dataService.isConsentPreferenceApplied.next({ requesttype: 'consentpreference', hasaccess: true })
+  //   }
+                // if (finalObj !== null && Object.keys(finalObj).length !== 0) {
+                //     return true;
+                // } else {
+                //     return false;
+                //     //this.router.navigate(['/home/dashboard/analytics'], { queryParams: { oid: oIDPIDFromURL[0], pid: oIDPIDFromURL[1] }, skipLocationChange: false });
+                // }
+            }
         }
 
     }
--- src/app/_services/licenseguardproperty.service.ts
+++ src/app/_services/licenseguardproperty.service.ts
@@ -1,55 +1,63 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { findPropertyIDFromUrl } from '../_helpers/common-utility';
 import { DataService } from '../_services/data.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class LicenseguardPropertyService implements CanActivate {
 
-    constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+    private router: Router,
+    private location: Location) { }
 
-    canActivate(): boolean {
-        let licenseStatusForProperty;
-      // on page refresh
-        let propPlandetails = JSON.stringify(this.dataService.getCurrentPropertyPlanDetails()); // on page refresh
-        let ispropplanExist;
-        if(JSON.parse(propPlandetails).response && JSON.parse(propPlandetails).response.plan_details &&  JSON.parse(propPlandetails).response.plan_details.cookieConsent){
-          if(Object.values(JSON.parse(propPlandetails).response.plan_details.cookieConsent).length > 0){
-            ispropplanExist = true;
+  async canActivate(): Promise<boolean> {
+    let licenseStatusForProperty;
+    // on page refresh
+    let propPlandetails = JSON.stringify(this.dataService.getCurrentPropertyPlanDetails()); // on page refresh
+    let ispropplanExist;
+    if (JSON.parse(propPlandetails).response && JSON.parse(propPlandetails).response.plan_details && JSON.parse(propPlandetails).response.plan_details.cookieConsent) {
+      if (Object.values(JSON.parse(propPlandetails).response.plan_details.cookieConsent).length > 0) {
+        ispropplanExist = true;
+      }
+    }
+    this.dataService.isLicenseAppliedForProperty.subscribe((status) => {
+      licenseStatusForProperty = status.hasaccess;
+    });
+
+    if (licenseStatusForProperty || ispropplanExist) {
+      return true;
+    } else {
+      let oIDPIDFromURL = findPropertyIDFromUrl(this.location.path())
+      if (oIDPIDFromURL !== undefined) {
+        let results = await this.dataService.checkLicenseAvailabilityForProperty(oIDPIDFromURL[1]).toPromise();
+
+        if (results.length > 0) {
+          let finalObj = {
+            ...results[0].response
           }
-        }
-        this.dataService.isLicenseAppliedForProperty.subscribe((status) => {
-            licenseStatusForProperty = status.hasaccess;
-        });
+          if (finalObj !== null && Object.keys(finalObj).length !== 0) {
+            if (finalObj !== undefined && finalObj.plan_details && finalObj.plan_details.cookieConsent) {
+              if (Object.values(finalObj.plan_details.cookieConsent).length > 0) {
+                return true;
+              } else {
+                this.dataService.openUpgradeModalForCookieConsent(results[0])
+                this.router.navigate(['/home/dashboard/analytics'], { queryParams: { oid: oIDPIDFromURL[0], pid: oIDPIDFromURL[1] }, skipLocationChange: false });
+                return false;
+              }
+            }
+          }
 
-        if (licenseStatusForProperty || ispropplanExist) {
-            return true;
         } else {
-            return false;
+          this.dataService.openUpgradeModalForCookieConsent(results[0])
+          this.router.navigate(['/home/dashboard/analytics'], { queryParams: { oid: oIDPIDFromURL[0], pid: oIDPIDFromURL[1] }, skipLocationChange: false });
+          return false;
         }
-
+      }
     }
 
+  }
+
 }
-
-// canActivate(): boolean {
-//     const currentUser = this.authenticationService.currentUserValue;
-//     let isPropertyCreated;
-//     this.dataService.OrganizationCreatedStatus.subscribe((status) => {
-//       isPropertyCreated = status;
-//     });
-//     if (currentUser) {
-//       const checkStatusOnPageRefresh = this.dataService.getOrganizationPropertyCreationStatus();
-//       if (!isPropertyCreated && typeof checkStatusOnPageRefresh !== 'undefined' || !isPropertyCreated && checkStatusOnPageRefresh !== null) { 
-//         return true;
-//       }
-//       if (isPropertyCreated) {
-//         return true;
-//       }
-//     } else {
-//       return false
-//     }
-
-
-//   }
