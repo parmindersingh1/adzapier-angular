9274f1d9da8f5d7c4c064227dee23f893e9d43d2
milanpanchal <mpanchal@adzapier.com>
2021-11-26 16:43:15 +0530

    #AP-2511 unauthorized acess for forgot password and system-integration page resolved,#AP-2513 system integration user routed to 404 page while logout resolved

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -20,7 +20,11 @@ export class ErrorInterceptor implements HttpInterceptor {
           }
           if (err.status === 400) {
             // auto logout if 401 response returned from api
-            if(err.url.indexOf('api/v1/invite/user/company') !== -1 && err.error.error.match("exist.") !== -1 || err.url.indexOf('/api/v1/login') == -1 && err.error.error.match("Incorrect") !== -1){//
+            if(err.url.indexOf('api/v1/integration') !== -1){
+               const error = err.error.error || err.statusText;
+               return throwError(error);
+             }
+            if(err.url.indexOf('api/v1/invite/user/company') !== -1 && err.error.error.match("exist.") !== null || err.url.indexOf('/api/v1/login') == -1 && err.error.error.match("Incorrect") !== null){//
               this.authenticationService.logout();
               this.dataService.openUnAuthModal.next({isTrue: true, error: err})
               this.router.navigate(['/error/unauthorized']);
@@ -34,7 +38,7 @@ export class ErrorInterceptor implements HttpInterceptor {
           if (err.status === 401) {
               // auto logout if 401 response returned from api
              // this.dataService.openUnAuthModal.next({isTrue: true, error: err})
-              if(err.url.indexOf('/api/v1/login') == -1 && err.error.error.match("exist.") == -1 || err.url.indexOf('/api/v1/login') == -1 && err.error.error.match("Incorrect") == -1){//
+              if(err.url.indexOf('/api/v1/login') == -1 && err.error.error.match("exist.") !== null || err.url.indexOf('/api/v1/login') == -1 && err.error.error.match("Incorrect") !== null){//
                 this.authenticationService.logout();
                 this.router.navigate(['/error/unauthorized']);
                 const error = err.error.error || err.statusText;
