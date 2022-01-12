cf4fb91fb6a33203af1693d95167ddd47b28848c
Aditya Mishra <amishra@adzapier.com>
2022-01-12 19:29:08 +0530

    fixed cookie banner issue #AP-2790

--- src/app/_helpers/formUnsaved.guard.ts
+++ src/app/_helpers/formUnsaved.guard.ts
@@ -6,7 +6,11 @@ export  interface HasUnsavedData {
 @Injectable()
 export class HasUnsavedDataGuard implements CanDeactivate<any> {
   canDeactivate(component: HasUnsavedData): boolean {
-    if (component.hasUnsavedData && component.hasUnsavedData()) {
+    try {
+      if (component.hasUnsavedData && component.hasUnsavedData()) {
+        return confirm('You have some unsaved form data. Are you sure, you want to leave this page?');
+      }
+    } catch (e){
       return confirm('You have some unsaved form data. Are you sure, you want to leave this page?');
     }
     return true;
